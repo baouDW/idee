babee07ea46844caee08bb5edf9fb8af144a57b5
baou <baou75@hotmail.com>
2019-04-06 00:44:09 +0200

    slider 60% fini

--- view/frontend/accueilView.php
+++ view/frontend/accueilView.php
@@ -41,73 +41,67 @@
 <script type="text/javascript">
   $(document).ready(function(){
     
-var $carrousel = $('#carrousel'), // on cible le bloc du carrousel
-    $img = $('#carrousel .text-slide'), // on cible les images contenues dans le carrousel
-    indexImg = $img.length - 1, // on définit l'index du dernier élément
-    i = 0, // on initialise un compteur
-    $currentImg = $img.eq(i); // enfin, on cible l'image courante, qui possède l'index i (0 pour l'instant)
+  var $carrousel = $('#carrousel'), 
+    $img = $('#carrousel .text-slide'), 
+    indexImg = $img.length - 1, 
+    i = 0, 
+    $currentImg = $img.eq(i); 
 
-$img.css('display', 'none'); // on cache les images
-$currentImg.css('display', 'block'); // on affiche seulement l'image courante
+  $img.css('display', 'none');
+  $currentImg.css('display', 'block');
 
 
 
-$('.next').click(function(){ // image suivante
+  $('.next').click(function(){ 
 
-    i++; // on incrémente le compteur
+    i++; 
 
     if( i <= indexImg ){
-        $img.css('display', 'none'); // on cache les images
-        $currentImg = $img.eq(i); // on définit la nouvelle image
-        $currentImg.css('display', 'block'); // puis on l'affiche
+      $img.css('display', 'none'); 
+      $currentImg = $img.eq(i); 
+      $currentImg.css('display', 'block'); 
     }
     else{
-        i = indexImg;
+      i = indexImg;
     }
 
 });
 
-$('.prev').click(function(){ // image précédente
-
-    i--; // on décrémente le compteur, puis on réalise la même chose que pour la fonction "suivante"
-
+  $('.prev').click(function(){ 
+    i--; 
     if( i >= 0 ){
-        $img.css('display', 'none');
-        $currentImg = $img.eq(i);
-        $currentImg.css('display', 'block');
+      $img.css('display', 'none');
+      $currentImg = $img.eq(i);
+      $currentImg.css('display', 'block');
     }
     else{
-        i = 0;
+      i = 0;
     }
-
 });
 
-function slideImg(){
-    setTimeout(function(){ // on utilise une fonction anonyme
+  function slideImg(){
+    setTimeout(function(){ 
             
-        if(i < indexImg){ // si le compteur est inférieur au dernier index
-      i++; // on l'incrémente
-  }
-  else{ // sinon, on le remet à 0 (première image)
-      i = 0;
-  }
+      if(i < indexImg){ 
+        i++; 
+      }
+      else{ 
+        i = 0;
+      }
 
   $img.css('display', 'none');
 
   $currentImg = $img.eq(i);
   $currentImg.css('display', 'block');
 
-  slideImg(); // on oublie pas de relancer la fonction à la fin
-
-    }, 7000); // on définit l'intervalle à 7000 millisecondes (7s)
+  slideImg();
+  
+  }, 7000); 
 }
 
-slideImg(); // enfin, on lance la fonction une première fois
+slideImg(); 
 
 });
-
-
-
 </script>
 
 
